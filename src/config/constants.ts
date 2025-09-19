3de1e44a192fbf0d27d6831e0d90dbdbc8ec2d4a
Claudio Uslenghi <claudio.uslenghi@gmail.com>
2025-09-19 14:25:22 -0300

    update urls

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
 // Configuration constants for the application
-export const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/a/macros/zircon.tech/s/AKfycby7PGv445MKUswcEuzBwCCEeL9OMH7lGEjzcFWiLvN1VC7Om8u3Sgrr6TW1Y9qC3LYX/exec';
+export const GOOGLE_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzQ_L2z8p68bCTftG03G9-SLhBIkt2rhI_Ip1i_21gwFdyPkgR-IgAUO7UZcEY8_3Cn/exec';
 
-export const GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSczGCvGvpeenWw2jzm--GpEz9X4HUQqh_7SmrzvWGYOsU5R-g/viewform';
+export const GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLScZRI6NlR_A-PHbpJksaDk8_c_adfxLCmh-syN3Mqlw2L_rYw/viewform';
